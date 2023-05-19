99eaca45e1180cd1ae128b7276ecfcb115ad4baf
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-05-19 13:22:38 +0100

    add title slug plugin

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -12,4 +12,27 @@ class CmsServiceProvider extends PluginServiceProvider
         $package
             ->name('cms');
     }
+
+    public function packageRegistered(): void
+    {
+        parent::packageRegistered();
+
+//        $this->app->singleton(FlatCmsSettings::class, function () {
+//            return FlatCmsSettings::make(storage_path('app/settings.json'));
+//        });
+
+        $this->loadMigrationsFrom([
+            __DIR__ . '/../database/migrations'
+        ]);
+
+//        $this->publishes([
+//            __DIR__ . '/../resources/views/layouts' => resource_path('views/vendor/cms/layouts'),
+//            __DIR__ . '/../resources/views/pages' => resource_path('views/vendor/cms/pages'),
+//            __DIR__ . '/../resources/views/posts' => resource_path('views/vendor/cms/posts'),
+//            __DIR__ . '/../resources/views/posts' => resource_path('views/vendor/cms/services'),
+//            __DIR__ . '/../resources/views/posts' => resource_path('views/vendor/cms/projects'),
+//            __DIR__ . '/../resources/views/posts' => resource_path('views/vendor/cms/events'),
+//            __DIR__ . '/../resources/views/components' => resource_path('views/vendor/cms/components'),
+//        ], 'cms.views');
+    }
 }
