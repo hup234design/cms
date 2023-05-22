4a0592dff55ce9a3961aca54f9132ea518ff04ac
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-05-22 17:17:24 +0100

    WIP

--- resources/views/components/app-header.blade.php
+++ resources/views/components/app-header.blade.php
@@ -1,5 +1,6 @@
 <header class="bg-gray-700 h-24 flex items-center justify-center">
     <span class="font-bold text-lg text-white">
+        <br>
         {{ config('app.name') }}
     </span>
 </header>
