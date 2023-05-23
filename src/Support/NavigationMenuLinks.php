edfa13c6fb056152605de0e0bbdfbff55f4aafbe
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-05-23 12:56:08 +0100

    WIP

--- src/Support/NavigationMenuLinks.php
+++ src/Support/NavigationMenuLinks.php
@@ -20,25 +20,25 @@ class NavigationMenuLinks
             } else {
                 switch($item['type']) {
                     case "page":
-                        if ($item['data']['slug'] == 'home' ) {
-                            $href = route('home');
-                        }
-                        elseif ($item['data']['slug'] == 'posts' ) {
-                            $href = route('posts');
-                        }
-                        else {
-                            $href = route('page', $item['data']['slug']);
-                        }
+//                        if ($item['data']['slug'] == 'home' ) {
+//                            $href = route('pages.home');
+//                        }
+//                        elseif ($item['data']['slug'] == 'posts' ) {
+//                            $href = route('posts.index');
+//                        }
+//                        else {
+                            $href = route('pages.page', $item['data']['slug']);
+//                        }
                         break;
-                    case "service":
-                        $href = route('service', $item['data']['slug']);
-                        break;
-                    case "project":
-                        $href = route('project', $item['data']['slug']);
-                        break;
-                    case "event":
-                        $href = route('event', $item['data']['slug']);
-                        break;
+//                    case "service":
+//                        $href = route('service', $item['data']['slug']);
+//                        break;
+//                    case "project":
+//                        $href = route('project', $item['data']['slug']);
+//                        break;
+//                    case "event":
+//                        $href = route('event', $item['data']['slug']);
+//                        break;
                     default:
                         $href = route($item['data']['slug']);
                         break;
