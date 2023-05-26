163532dc8c2b1baf63bd3fc2eac5dc3aa8440263
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-05-26 14:06:30 +0100

    WIP

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,16 @@ use Hup234design\Cms\Http\Controllers\PageController;
 use Hup234design\Cms\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/events/category/{slug}', [EventController::class, 'category'])->name('events.category');
-Route::get('/events/{slug}', [EventController::class, 'event'])->name('events.event');
-Route::get('/events', [EventController::class, 'index'])->name('events');
+Route::middleware(['web'])->group(function () {
+    Route::get('/events/category/{slug}', [EventController::class, 'category'])->name('events.category');
+    Route::get('/events/{slug}', [EventController::class, 'event'])->name('events.event');
+    Route::get('/events', [EventController::class, 'index'])->name('events');
 
-Route::get('/posts/category/{slug}', [PostController::class, 'category'])->name('posts.category');
-Route::get('/posts/{slug}', [PostController::class, 'post'])->name('posts.post');
-Route::get('/posts', [PostController::class, 'index'])->name('posts');
+    Route::get('/posts/category/{slug}', [PostController::class, 'category'])->name('posts.category');
+    Route::get('/posts/{slug}', [PostController::class, 'post'])->name('posts.post');
+    Route::get('/posts', [PostController::class, 'index'])->name('posts');
 
-Route::get('/{slug}', [PageController::class, 'page'])->name('pages.page');
-Route::get('/', [PageController::class, 'home'])->name('home');
+    Route::get('/{slug}', [PageController::class, 'page'])->name('pages.page');
+    Route::get('/', [PageController::class, 'home'])->name('home');
+});
+
