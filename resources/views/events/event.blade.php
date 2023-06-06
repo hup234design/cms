4b6c12c0fb6a25ca769c13dc9a5032f57a7e926c
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-06-06 09:54:02 +0100

    WIP

--- resources/views/events/event.blade.php
+++ resources/views/events/event.blade.php
@@ -1,8 +1,5 @@
 <x-cms-events-layout>
-    <div class="prose max-w-none">
-        <h1>
-            {{ $record->title }}
-        </h1>
-        {!! $record->content !!}
-    </div>
+
+    @include('cms::components.content')
+
 </x-cms-events-layout>
