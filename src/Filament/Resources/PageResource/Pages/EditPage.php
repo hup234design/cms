261d079eceaecca198da9dda7a6c641d365f2040
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-06-06 11:12:17 +0100

    WIP

--- src/Filament/Resources/PageResource/Pages/EditPage.php
+++ src/Filament/Resources/PageResource/Pages/EditPage.php
@@ -3,26 +3,16 @@
 namespace Hup234design\Cms\Filament\Resources\PageResource\Pages;
 
 use Hup234design\Cms\Filament\Resources\PageResource;
-use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
-use Pboivin\FilamentPeek\Pages\Actions\PreviewAction;
-use Pboivin\FilamentPeek\Pages\Concerns\HasPreviewModal;
 
 class EditPage extends EditRecord
 {
-    use HasPreviewModal;
-
     protected static string $resource = PageResource::class;
 
     protected function getActions(): array
     {
         return [
-            PreviewAction::make()->color('success'),
+            //
         ];
-    }
-
-    protected function getPreviewModalView(): ?string
-    {
-        return 'cms::components.preview';
     }
 }
--- src/Filament/Resources/PostResource/Pages/EditPost.php
+++ src/Filament/Resources/PostResource/Pages/EditPost.php
@@ -3,26 +3,16 @@
 namespace Hup234design\Cms\Filament\Resources\PostResource\Pages;
 
 use Hup234design\Cms\Filament\Resources\PostResource;
-use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
-use Pboivin\FilamentPeek\Pages\Actions\PreviewAction;
-use Pboivin\FilamentPeek\Pages\Concerns\HasPreviewModal;
 
 class EditPost extends EditRecord
 {
-    use HasPreviewModal;
-
     protected static string $resource = PostResource::class;
 
     protected function getActions(): array
     {
         return [
-            PreviewAction::make()->color('success'),
+            //
         ];
-    }
-
-    protected function getPreviewModalView(): ?string
-    {
-        return 'cms::components.preview';
     }
 }
