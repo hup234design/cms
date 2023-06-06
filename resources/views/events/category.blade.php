75df2a1eb242f3b5964acd8a7b5ef55227ddac82
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-06-06 09:55:29 +0100

    WIP

--- resources/views/events/category.blade.php
+++ resources/views/events/category.blade.php
@@ -1,3 +1,19 @@
 <x-cms-events-layout>
-    @section('headingSubTitle', $category->title)
+    @section('headingSubtitle', $category->title)
+
+    <div class="lg:flex-1 prose max-w-none">
+        @forelse($events as $event)
+            <h3>
+                {{ $event->title }}
+            </h3>
+            <p>
+                {{ nl2br($event->summary) }}
+            </p>
+            <p>
+                <a href="{{ route('events.event', $event->slug) }}">Read More</a>
+            </p>
+        @empty
+            <p>no events</p>
+        @endforelse
+    </div>
 </x-cms-events-layout>
