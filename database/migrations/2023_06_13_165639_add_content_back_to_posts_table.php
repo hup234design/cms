afd2565b5e3612e01a655ff8373beda6dfb4c770
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-06-13 18:01:26 +0100

    Update 2023_06_13_165639_add_content_back_to_posts_table.php
    typo

--- database/migrations/2023_06_13_165639_add_content_back_to_posts_table.php
+++ database/migrations/2023_06_13_165639_add_content_back_to_posts_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('content'):q;
+            $table->dropColumn('content');
         });
     }
 };
