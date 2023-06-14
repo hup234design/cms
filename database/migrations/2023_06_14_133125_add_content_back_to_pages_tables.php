750443e7441bf8f83d130e21c327a7dbcb01a3b3
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2023-06-14 14:33:10 +0100

    add content back to pages

---
+++ database/migrations/2023_06_14_133125_add_content_back_to_pages_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->longText('content')->after('slug')->nullable();
+        });
+
+        Schema::table('index_pages', function (Blueprint $table) {
+            $table->longText('content')->after('slug')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('index_pages', function (Blueprint $table) {
+            $table->dropColumn('content');
+        });
+
+        Schema::table('pages', function (Blueprint $table) {
+            $table->dropColumn('content');
+        });
+    }
+};
